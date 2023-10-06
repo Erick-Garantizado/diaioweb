ad03428ceb7fcdabe706821fd7d72d45fd34a7f5
erick note <garantizadoerick@gmail.com>
2023-10-06 15:34:39 -0400

    criado o controller de depoimentos

---
+++ backend/controllers/DepoimentosController.js
@@ -0,0 +1,9 @@
+class DepoimentosController {
+    static async salvar(req, res) {
+        const titulo = req.body.titulo;
+        const mensagem = req.body.mensagem;
+
+    }
+}
+
+module.exports = DepoimentosController
